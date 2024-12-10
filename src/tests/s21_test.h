4ff7dd69f385d6a32e2c5df8efea6dfa867249c9
Святослав Заикин <sviatoslavzz@MacBook-Air-Svatoslav.local>
2024-12-10 19:56:56 +0300

    relocate to org

---
+++ src/tests/s21_test.h
@@ -0,0 +1,22 @@
+#include <check.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../s21_math.h"
+
+Suite *s21_abs_test_suite();
+Suite *s21_asin_test_suite();
+Suite *s21_atan_test_suite();
+Suite *s21_ceil_test_suite();
+Suite *s21_cos_test_suite();
+Suite *s21_fabs_test_suite();
+Suite *s21_floor_test_suite();
+Suite *s21_fmod_test_suite();
+Suite *s21_pow_test_suite();
+Suite *s21_sin_test_suite();
+Suite *s21_sqrt_test_suite();
+Suite *s21_tan_test_suite();
+Suite *s21_acos_test_suite();
+Suite *s21_log_test_suite();
+Suite *s21_exp_test_suite();
